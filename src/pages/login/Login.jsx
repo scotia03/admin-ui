f5fc66373e8eff8712052a04159f7ca4affbdcae
hilmis <115560028+scotia03@users.noreply.github.com>
2024-04-18 08:37:27 +0700

    ubah teks di app

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,29 +1,6 @@
-import { useState } from "react";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import List from "./pages/list/List";
-import Single from "./pages/single/Single";
-import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./login.scss";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />}></Route>
-            <Route path="login" element={<Login />}></Route>
-            <Route path="users">
-              <Route index element={<List />}></Route>
-              <Route path=":userId" element={<Single />}></Route>
-              <Route path="new" element={<New />}></Route>
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-}
-
-export default App;
+const login = () => {
+    return <div>login</div>;
+};
+export default login;
